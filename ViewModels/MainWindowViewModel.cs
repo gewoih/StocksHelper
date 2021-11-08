ef5eb6ea36072b4bad225d173234e7d1b601ab86
Никита Раненко <nranenko@bk.ru>
2021-11-08 10:31:38 +0300

    Рефакторинг программы под Телеграм бота, удаление ненужных команд, методов и файлов

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -29,17 +29,11 @@ namespace StocksHelper.ViewModels
 		#region Constructor
 		public MainWindowViewModel()
 		{
-			this.NewStock = new Stock();
 			this.DBContext = new BaseDataContext();
-			this.MainContentControl = new AuthenticationViewModel(this);
 			this._UsersRepository = new UsersRepository(this.DBContext);
 			this._StocksRepository = new StocksRepository(this.DBContext);
 			this._StockQuotesRepository = new StocksQuotesRepository(this.DBContext);
 			this.BotClient = new TelegramBotClient(ConfigurationManager.AppSettings["TelegramBotAPI"]);
-
-			ShowNewStockWindowCommand = new RelayCommand(OnShowNewStockWindowCommandExecuted, CanShowNewStockWindowCommandExecute);
-			AddStockCommand = new RelayCommand(OnAddStockCommandExecuted, CanAddStockCommandExecute);
-			RemoveStockCommand = new RelayCommand(OnRemoveStockCommandExecuted, CanRemoveStockCommandExecute);
 
 			this.LoadMissingQuotes();
 			this.ConfigureTelegramBot(this.BotClient);
@@ -47,103 +41,15 @@ namespace StocksHelper.ViewModels
 		#endregion
 
 		#region Properties
-		private object _mainContentControl;
-		public object MainContentControl
-		{
-			get => _mainContentControl;
-			set => Set(ref _mainContentControl, value);
-		}
-
 		private readonly BaseDataContext DBContext;
 		private UsersRepository _UsersRepository;
-		private IRepository<Stock> _StocksRepository;
-		private IRepository<StockQuote> _StockQuotesRepository;
-
-		private User _LoggedInUser;
-		public User LoggedInUser
-		{
-			get => _LoggedInUser;
-			set
-			{
-				Set(ref _LoggedInUser, value);
-				this.MainContentControl = null;
-			}
-		}
-
-		private Stock _SelectedStock;
-		public Stock SelectedStock
-		{
-			get => _SelectedStock;
-			set
-			{
-				Set(ref _SelectedStock, value);
-				this.MainContentControl = new StocksViewModel(this.SelectedStock);
-			}
-		}
-
-		private Stock _NewStock;
-		public Stock NewStock
-		{
-			get => _NewStock;
-			set => Set(ref _NewStock, value);
-		}
+		private StocksRepository _StocksRepository;
+		private StocksQuotesRepository _StockQuotesRepository;
 
 		TelegramBotClient BotClient;
 		#endregion
 
-		#region Commands
-		//Вывод окна для добавления новой акции
-		public ICommand ShowNewStockWindowCommand { get; }
-		private bool CanShowNewStockWindowCommandExecute(object p) => this.LoggedInUser != null;
-		public void OnShowNewStockWindowCommandExecuted(object p)
-		{
-			NewStockWindow newWindow = new NewStockWindow(this);
-			newWindow.ShowDialog();
-		}
-
-		//Добавление новой акции к аккаунту пользователя
-		public ICommand AddStockCommand { get; }
-		private bool CanAddStockCommandExecute(object p) => this.LoggedInUser != null;
-		public void OnAddStockCommandExecuted(object p)
-		{
-			//Ищем акцию с таким тикером в БД
-			Stock newStock = this._StocksRepository.GetAll().FirstOrDefault(s => s.Symbol.ToUpper() == this.NewStock.Symbol.ToUpper());
-			Stock createdStock;
-
-			//Если находим - делаем связку пользователя и найденной акции
-			if (newStock != null)
-			{
-				if (this.LoggedInUser.Stocks.Count(s => s.Symbol.ToUpper() == this.NewStock.Symbol.ToUpper()) == 0)
-					AddStockToUser(newStock);
-				else
-					MessageBox.Show("Данная акция уже числится на вашем аккаунте.");
-			}
-			//Если не находим акцию в БД - ищем такой тикер через YahooFinance и выполняем связку
-			else if ((createdStock = YahooCreateStockBySymbol(this.NewStock.Symbol)) != null)
-				AddStockToUser(createdStock);
-			//Не находим такой тикер на YahooFinance - сообщение об ошибке
-			else
-				MessageBox.Show("Данный тикер не найден.");
-		}
-
-		//Удаление выбранной акции с аккаунта пользователя
-		public ICommand RemoveStockCommand { get; }
-		private bool CanRemoveStockCommandExecute(object p) => this.SelectedStock != null;
-		public void OnRemoveStockCommandExecuted(object p)
-		{
-			DialogResult dialogResult = MessageBox.Show($"Вы действительно хотите удалить акцию {SelectedStock.Name}[{SelectedStock.Symbol}]?",
-																	"Удаление акции",
-																	MessageBoxButtons.YesNo);
-
-			if (dialogResult == DialogResult.Yes)
-			{
-				//Сначала удаляем акцию из БД, затем из программы
-				this._UsersRepository.RemoveStock(this.LoggedInUser.Id, this.SelectedStock.Id);
-				this.LoggedInUser.Stocks.Remove(this.SelectedStock);
-
-				MessageBox.Show("Акция удалена.");
-			}
-		}
+		#region Commands		
 		#endregion
 
 		#region Methods
@@ -164,14 +70,6 @@ namespace StocksHelper.ViewModels
 				return null;
 			//Найденную акцию сразу добавляем в БД и возвращаем ее результатом метода для дальнейшей обработки
 			return this._StocksRepository.Create(new Stock { Symbol = symbol.ToUpper(), Name = result[0].quote.shortName });
-		}
-
-		//Привязка акции к аккаунту пользователя
-		private void AddStockToUser(Stock stock)
-		{
-			this.LoggedInUser.Stocks.Add(stock);
-			this._UsersRepository.AddStock(this.LoggedInUser, stock);
-			MessageBox.Show($"Бумага {stock.Symbol} успешно добавлена!");
 		}
 
 		private async void SendNotifications(Stock stock)
